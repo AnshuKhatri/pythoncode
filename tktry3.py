7048a2a2e8b1fa3a1dcaee9bbd23cfac28525eb6
AnshuKhatri <anshukhatri03@gmail.com>
2017-04-02 18:53:48 +0530

    Tkinter example practice

---
+++ tktry3.py
@@ -0,0 +1,50 @@
+import Tkinter
+
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
+        self.entry.grid(column=2,row=1,sticky='EW')
+        #self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u" Title ... ")
+
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
+        self.entry.grid(column=2,row=5,sticky='EW')
+       # self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u" Summary ...")
+
+        
+
+        self.labelVariable = Tkinter.StringVar()
+        label = Tkinter.Label(self,textvariable=self.labelVariable,
+                              anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=1,columnspan=2,sticky='EW')
+        self.labelVariable.set(u"Title")
+
+        self.labelVariable = Tkinter.StringVar()
+        label = Tkinter.Label(self,textvariable=self.labelVariable,
+                              anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=5,columnspan=2,sticky='EW')
+        self.labelVariable.set(u"Summary")
+
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,False)
+        self.update()
+        self.geometry(self.geometry())       
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+   
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('my application')
+    app.mainloop()
